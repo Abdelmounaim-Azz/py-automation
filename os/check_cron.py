e070965e11ba2890694f61e99883650e20fbfdde
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-09-21 10:55:49 +0100

    improve check_cron script

--- os/check_cron.py
+++ os/check_cron.py
@@ -1,11 +1,16 @@
 import sys
 import re
 
-logfile=sys.argv[1]
+logfile = sys.argv[1]
+usernames = {}
 with open(logfile) as f:
-  for line in f:
-    if "CRON" not in line:
-      continue
-    pattern=r"USER \((\w+)\)$"
-    res=re.search(pattern,logfile)
-    print(res[1])
+    for line in f:
+        if "CRON" not in line:
+            continue
+        pattern = r"USER \((\w+)\)$"
+        res = re.search(pattern, logfile)
+        if res is None:
+            continue
+        name = res[1]
+        usernames[name] = usernames.get(name, 0)+1
+print(usernames)
