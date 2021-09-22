28a7f91da311f778ab063d46d57668fe5204a1d3
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-09-22 09:03:06 +0100

    simple one

--- os/usage_statistics.py
+++ os/usage_statistics.py
@@ -8,11 +8,10 @@ with open(logfile) as f:
         pattern = r"\((\w+)\)$"
         res = re.search(pattern, line)
         name = res[1]
-        users[name] = set()
         errP = r"ERROR: "
         error = re.search(errP, line)
         if error is None:
-            users[name].add({"Info": users[name].get("Info", 0)+1})
+            users[name] = users.get(name, 0)+1
         else:
-            users[name].add({"Erro": users[name].get("Error", 0)+1})
+            users[name] = users.get(name, 0) + 1
 print(sorted(users.items()))
