b26dddeb5d2d2447f15ff6b958ddf56b74ff174a
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-09-22 11:53:05 +0100

    usage stats fixed

--- os/usage_statistics.py
+++ os/usage_statistics.py
@@ -2,18 +2,20 @@ import sys
 import re
 
 logfile = sys.argv[1]
+error = {}
 users = {}
 with open(logfile) as f:
     for line in f:
         pattern = r"\((\w+)\)$"
         res = re.search(pattern, line)
         name = res[1]
+        user_err = {'Info': 0, 'Error': 0}
+        if name not in users:
+            users[name] = user_err
         errP = r"ERROR: "
         error = re.search(errP, line)
         if error is None:
-            users[name] = users.get(name, {"Info": 0})
             users[name]["Info"] += 1
-        else:
-            users[name] = users.get(name, {"Error": 0})
+        elif "ERROR" in line:
             users[name]["Error"] += 1
 print(sorted(users.items()))
